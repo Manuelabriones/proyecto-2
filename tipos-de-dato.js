5ce3cf25ccb9feadb58b7c9dad0a6ff0bb80f823
unknown <st4603@utr.edu.mx>
2026-04-20 14:32:18 -0600

    Agrega tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,18 @@
+// Tipos de datos en JavaScript
+
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+
+// Este da error (a propósito, puedes comentarlo)
+// console.log(typeof FALSE);
+
+// Ejemplos extra
+console.log(typeof "Hola mundo");
+console.log(typeof 3.14);
+console.log(typeof false);
