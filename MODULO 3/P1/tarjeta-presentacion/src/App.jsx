aa36f87c741d83a68015bfa5fdbc9f6d51798fb1
unknown <st4603@utr.edu.mx>
2026-07-26 17:56:07 -0600

    Corregir proyecto React P1

---
+++ MODULO 3/P1/tarjeta-presentacion/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tarjeta from "./components/Tarjeta";
+
+function App() {
+  return (
+    <div className="container">
+      <Tarjeta />
+    </div>
+  );
+}
+
+export default App;
---
+++ MODULO 3/P1/tarjeta-presentacion/src/components/Tarjeta.jsx
@@ -0,0 +1,14 @@
+function Tarjeta() {
+  return (
+    <div className="card">
+      <h1>Manuela Briones Vela</h1>
+      <h2>Software Development Student</h2>
+      <p>
+        Hello! My name is Manuela. I am learning React and I enjoy creating
+        websites and web applications.
+      </p>
+    </div>
+  );
+}
+
+export default Tarjeta;
